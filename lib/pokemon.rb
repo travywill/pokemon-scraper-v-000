054ca4fd3d70ac0d998fd712d669c1357a62149f
Travis Williamson <travywill@gmail.com>
2019-01-03 21:51:01 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,7 +15,7 @@ class Pokemon
     @@all
   end
 
-  def self.save(name:, type:, db:)
+  def self.save(name, type, db)
     @db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
   end
 
