5653575a217f4315f8f40b190ded33022b454be9
Travis Williamson <travywill@gmail.com>
2019-01-03 20:17:15 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,16 @@
 class Pokemon
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def save
+    #
+  end
+
+  def find
+    #
+  end
 end
