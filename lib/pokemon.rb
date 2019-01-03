ff7c5e725ecb87cb79f2c520092796b7b42a61d5
Travis Williamson <travywill@gmail.com>
2019-01-03 22:28:45 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,6 +20,6 @@ class Pokemon
   end
 
   def self.find(id, db)
-    db.execute("SELECT * FROM pokemon WHERE id = (?)", id).flatten(1)
+    pokemon_array = db.execute("SELECT * FROM pokemon WHERE id = (?)", id).flatten(1)
   end
 end
